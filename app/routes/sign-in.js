7790e2baf6a435b3fcc4514561cbf63c687a8386
AlexisDiaz8706 <70243744+AlexisDiaz8706@users.noreply.github.com>
2020-11-11 18:02:59 -0800

    Update sign-in.js

--- app/routes/sign-in.js
+++ app/routes/sign-in.js
@@ -1,4 +1,20 @@
-import Route from '@ember/routing/route';
+<NavBar></NavBar>
+<style>
+  .signup-button p {
+    line-height: 1.5;
+    text-align: center;
+    color: #fefefe;
+    margin-top: 10%;
+  }
+</style>
 
-export default class SignInRoute extends Route {
-}
+
+<h1>
+  Sign In
+</h1>
+<div class=" signup-button">
+  <LinkTo @route="sign-up"><p>Not registered yet? Sign up here</p></LinkTo>
+</div>
+
+
+{{outlet}}
